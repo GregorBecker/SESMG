81e346797646c580a423cabdd51cba4dd9b18b48
Gregor Becker <gregor.becker@fh-muenster.de>
2023-08-17 17:01:49 +0200

    Update hook-plotly.py

--- hooks/hook-plotly.py
+++ hooks/hook-plotly.py
@@ -3,3 +3,4 @@ from PyInstaller.utils.hooks import collect_data_files
 hiddenimports = collect_submodules('plotly')
 
 datas = collect_data_files('plotly', include_py_files=True)
+datas = collect_data_files('_plotly_utils', include_py_files=True)
